e40e6675e932953c905bfe5c0e7e08cf993e77b0
Darodrian <alon.gonzalezs@gmail.com>
2023-03-02 11:12:38 -0300

    Fix errores de consola, ^package.json, +context, +loading spinner component, -modo estricto

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from '../../assets/logoblanco.png'
-
+import React, { Component } from "react";
+import logo from "../../assets/logoblanco.png";
 
 export default class Footer extends Component {
-
-    render() {
-        return (
-
-            <footer class="bg-dark text-center text-white footer">
-
-                <div class="container p-4 pb-0">
-                    <section class="mb-4">
-                            <img class="logo" src={logo} alt="Cas-Chile Logo" />
-                    </section>
-                </div>
-                <hr />
-                <div class="text-center p-3 pb-4" >
-                    © 2023 Copyright: Cas-Chile S.A de I.
-                </div>
-
-            </footer>
-        )
-    }
-}               
+  render() {
+    return (
+      <footer className="bg-dark text-center text-white footer">
+        <div className="container p-4 pb-0">
+          <section className="mb-4">
+            <img className="logo" src={logo} alt="Cas-Chile Logo" />
+          </section>
+        </div>
+        <hr />
+        <div className="text-center p-3 pb-4">
+          © 2023 Copyright: Cas-Chile S.A de I.
+        </div>
+      </footer>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./style/main.css";
 import App from "./components";
 
+import { AuthProvider } from "./components/auth-provider";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
+  <AuthProvider>
     <App />
-  </React.StrictMode>
+  </AuthProvider>
+  //</React.StrictMode>
 );
